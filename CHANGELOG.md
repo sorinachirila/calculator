413159c3f29375655aa6428f56dad648c4613b57
sorinachirila <chirila_sorina@yahoo.com>
2022-12-02 17:15:24 +0200

    Added new file

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
 "Initial version of the Calculator project" 
 "#2020-12-11" 
 "* Added integer addition feature" 
+"Another change" 
--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,17 @@
 public class Calculator {
     public int addition(int a, int b){
 	return a + b;
+    }
+    
+    public double addition(double a, double b){
+        return a + b;
+    }
+    
+    public int multiply(int a, int b){
+        return a * b;
     } 
+
+    public double multiply(double a, double b){
+	return a * b;
+    }
 }
