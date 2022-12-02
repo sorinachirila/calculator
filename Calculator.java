435381d86900a483bf4985792a7f05992d547cf2
sorinachirila <chirila_sorina@yahoo.com>
2022-12-02 16:36:26 +0200

    Revert "Added a feature to multiply integer and decimal numbers"
    This reverts commit e9165a7bb496fc589b093e8f754262a770786b1d.

--- Calculator.java
+++ Calculator.java
@@ -5,13 +5,5 @@ public class Calculator {
     
     public double addition(double a, double b){
         return a + b;
-    }
-    
-    public int multiply(int a, int b){
-        return a * b;
     } 
-
-    public double multiply(double a, double b){
-	return a * b;
-    }
 }
